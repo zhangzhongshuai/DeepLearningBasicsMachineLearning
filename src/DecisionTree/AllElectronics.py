9fcee9e05dd0edb94b05324abbc356a752957459
Peter <peter_shuai@yeah.net>
2017-05-11 18:08:50 +0800

    KNN实现，好牛逼

--- src/DecisionTree/AllElectronics.py
+++ src/DecisionTree/AllElectronics.py
@@ -21,6 +21,7 @@ for row in reader:
     for i in range(1,len(row) - 1):
         rowDict[headers[i]]=row[i]
     featureList.append(rowDict)
+print(labelList)
 print(featureList)
 
 vec=DictVectorizer()
